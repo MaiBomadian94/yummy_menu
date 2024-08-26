32071f50b474309d7621eeaab926c0cfe1bf46d1
Mai Bomadian <101835325+MaiBomadian94@users.noreply.github.com>
2024-08-26 13:59:24 +0300

    our commit message

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 import '../main.dart';
 
+
+
+
+
 class Constants {
   static var theme = Theme.of(navigatorKey.currentState!.context);
   static var mediaQuery =
